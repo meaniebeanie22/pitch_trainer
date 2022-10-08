47e41f2b978189c3987dda8c77a895980ecf5275
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2022-10-08 16:14:59 +1100

    it actually works!

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import PySimpleGUI as sg
 import random
 from playsound import playsound
+# voodoo magic
+QT_ENTER_KEY1 = 'special 16777220'
+QT_ENTER_KEY2 = 'special 16777221'
+
+#score tracker
+tries = 0
+correct = 0
 
 # All the stuff inside your window.
 layout = [  [sg.Text('Type the note you hear')],
             [sg.Text('Note:'), sg.InputText(key='-IN-')],
-            [sg.Button('Play Again'), sg.Button('Submit')],
-            [sg.Text('Answer:'), sg.Text(key='-OUTPUT-')] ]
+            [sg.Button('Play Again')],
+            [sg.Text('Answer:'), sg.Text(key='-OUTPUT-'), sg.Text('0/0', key='-SCORE-', justification='right')] ]
 note_dic = {
     'a.mp3':['a'],
     'bflat.mp3':['bb','a#'],
@@ -23,7 +30,7 @@ note_dic = {
 }
 
 # Create the Window
-window = sg.Window('Pitch Trainer', layout)
+window = sg.Window('Pitch Trainer', layout, return_keyboard_events=True)
 
 # pick a note to start
 note = random.choice(list(note_dic.keys()))
@@ -34,14 +41,16 @@ while True:
     event, values = window.read() #get events and values
     if event == sg.WIN_CLOSED: # if user closes window
         break
-    if event == 'Submit': # check answer
+    if event in ('\r', QT_ENTER_KEY1, QT_ENTER_KEY2): # check answer
         if values['-IN-'] in note_dic[note]: # correct
             window['-OUTPUT-'].update('Correct!') # change text to give dopamine
-            note = random.choice(list(note_dic.keys())) # pick new note
-            playsound(note, False)
-            
-        else: # nope
+            correct += 1
+        else:
             window['-OUTPUT-'].update('Nope!')
+        tries += 1
+        window['-SCORE-'].update((str(correct) + '/' + str(tries)))
+        note = random.choice(list(note_dic.keys())) # pick new note
+        playsound(note, False)
         window['-IN-'].update('')
 
     if event == 'Play Again': # play sound again
