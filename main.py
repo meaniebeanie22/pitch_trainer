c4e6883a5eeadc9c5979fa038f5961390f83d219
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2022-10-10 13:05:15 +1100

    still stuffed - fix later

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ layout = [  [sg.Text('Type the note you hear')],
             [sg.Text('Note:'), sg.InputText(key='-IN-')],
             [sg.Button('Play Again'), sg.Button('Play C')],
             [sg.Text('Answer:'), sg.Text(key='-OUTPUT-'), sg.Text('0/0', key='-SCORE-', justification='right')] ]
-note_dic = {
+file_to_note = {
     'a.mp3':['a'],
     'bflat.mp3':['bb','a#'],
     'b.mp3':['b','cb'],
@@ -28,12 +28,35 @@ note_dic = {
     'g.mp3':['g'],
     'aflat.mp3':['g#','ab']
 }
+note_to_file = {
+    'a':'a.mp3',
+    'bb':'bflat.mp3',
+    'a#':'bflat.mp3',
+    'b':'b.mp3',
+    'cb':'b.mp3',
+    'c':'c.mp3',
+    'b#':'c.mp3',
+    'c#':'csharp.mp3',
+    'db':'csharp.mp3',
+    'd':'d.mp3',
+    'eb':'eflat.mp3',
+    'd#':'eflat.mp3',
+    'e':'e.mp3',
+    'fb':'e.mp3',
+    'f':'f.mp3',
+    'e#':'f.mp3',
+    'f#':'fsharp.mp3',
+    'gb':'fsharp.mp3',
+    'g':'g.mp3',
+    'g#':'aflat.mp3',
+    'ab':'aflat.mp3'
+}
 
 # Create the Window
 window = sg.Window('Pitch Trainer', layout, return_keyboard_events=True)
 
 # pick a note to start
-note = random.choice(list(note_dic.keys()))
+note = random.choice(list(file_to_note.keys()))
 playsound(note, False) # play it
 
 # Event Loop to process "events" and get the "values" of the inputs
@@ -43,19 +66,25 @@ while True:
         break
 
     if event in ('\r', QT_ENTER_KEY1, QT_ENTER_KEY2): # check answer
-        if values['-IN-'] in note_dic[note]: # correct
+        if values['-IN-'] in file_to_note[note]: # correct
             window['-OUTPUT-'].update('Correct!') # change text to give dopamine
             correct += 1
+            
         else:
-            window['-OUTPUT-'].update('Nope!')
+            message = 'Nope! You chose', values['-IN-'], 'It was a ' + '/'.join(file_to_note[note])
+            window['-OUTPUT-'].update(message)
+            playsound(note_to_file[values['-IN-']])
+            playsound(note)
+            
         tries += 1
         window['-SCORE-'].update((str(correct) + '/' + str(tries)))
-        note = random.choice(list(note_dic.keys())) # pick new note
-        playsound(note, False)
+        note = random.choice(list(file_to_note.keys())) # pick new note
         window['-IN-'].update('')
+        playsound(note)
+        
 
     if event == 'Play Again': # play sound again
-        playsound(note, False)
+        playsound(note)
     if event == 'Play C': #play c (for a baseline if wus)
-        playsound('c.mp3', False)
+        playsound('c.mp3')
 window.close()
